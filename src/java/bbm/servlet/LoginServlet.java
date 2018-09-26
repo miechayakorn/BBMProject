9f34a6f7588a5233bc6a93934c83c2e851cdb8b2
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-09-26 16:34:24 +0700

    [Add] new Project
    -add login Servlet

---
+++ src/java/bbm/servlet/LoginServlet.java
@@ -0,0 +1,121 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbm.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ *
+ * @author Acer_E5
+ */
+public class LoginServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+    String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        HttpSession session = request.getSession(false);
+
+        if (username != null && password != null && username.length() > 0 && password.length() > 0) {
+            AccountJpaController accJPA = new AccountJpaController(utx, emf);
+            Account acc = accJPA.findAccount(username);
+
+            password = cryptWithMD5(password);
+            if (acc != null) {
+                if (acc.getPassword().equals(password)) {
+
+                    if (session == null) {
+                        session = request.getSession(true);
+                    }
+                    session.setAttribute("account", acc);
+                    response.sendRedirect("newUrl");
+                    return;
+                } else {
+                    request.setAttribute("message", "Invalid user name or password !!");
+                }
+            } else {
+                request.setAttribute("message", "Invalid user name or password !!");
+            }
+        }
+        getServletContext().getRequestDispatcher("/Login.jsp").forward(request, response);
+
+    }
+    
+    public static String cryptWithMD5(String pass) {
+        try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] passBytes = pass.getBytes();
+            md.reset();
+            byte[] digested = md.digest(passBytes);
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < digested.length; i++) {
+                sb.append(Integer.toHexString(0xff & digested[i]));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
