d6ac41cc0023304f87942c7f5720f13b1bb52557
Mie Chayakorn <chayakorn.mie@mail.kmutt.ac.th>
2018-10-06 21:27:50 +0700

    [Create model] EncryptWithMd5

---
+++ src/java/bbm/model/EncryptWithMd5.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bbm.model;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ *
+ * @author Acer_E5
+ */
+public class EncryptWithMd5 {
+
+    public EncryptWithMd5() {
+    }
+
+    public String encrypt(String pass) {
+         try {
+            MessageDigest md = MessageDigest.getInstance("MD5");
+            byte[] passBytes = pass.getBytes();
+            md.reset();
+            byte[] digested = md.digest(passBytes);
+            StringBuffer sb = new StringBuffer();
+            for (int i = 0; i < digested.length; i++) {
+                sb.append(Integer.toHexString(0xff & digested[i]));
+            }
+            return sb.toString();
+        } catch (NoSuchAlgorithmException ex) {
+            System.out.println(ex);
+        }
+        return null;
+    }
+    
+    
+}
